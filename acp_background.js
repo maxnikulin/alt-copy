dcd250648df9001b1191e3658387fc92e8f626da
Max Nikulin <manikulin@gmail.com>
2024-06-06 13:15:24 +0700

    Update copyright year

--- acp_background.js
+++ acp_background.js
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-/* Copyright (C) 2021-2023 Max Nikulin */
+/* Copyright (C) 2021-2024 Max Nikulin */
 
 "use strict";
 
